060b6c7efb5faa4b8492e664873b8ad58b2e163f
VARDHAN REDDY <110156702+Vardhanreddy-17@users.noreply.github.com>
2026-02-11 06:17:46 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (72.50%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -3,12 +3,7 @@ class Solution {
         if(s.length()!=goal.length()){
             return false;
         }
-        for(int i=0;i<s.length();i++){
-            String rotated = s.substring(i)+s.substring(0,i);
-            if(rotated.equals(goal)){
-                return true;
-            }
-        }
-        return false;
+        String doubles = s+s;
+        return doubles.contains(goal);
     }
 }
